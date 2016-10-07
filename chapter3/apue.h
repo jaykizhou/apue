1992b89db146b84b340e5ece7613219fa11cd8ed
jaykizhou <yiqianniyiye@126.com>
2016-10-07 10:42:53 +0800

    示例3-4，5

--- chapter3/apue.h
+++ chapter3/apue.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 
 #define MAXLINE 4096
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IXOTH)
 
 void err_quit(const char *, ...) __attribute__((noreturn));
 void err_sys(const char *, ...) __attribute__((noreturn));
