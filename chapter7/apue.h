01221716482a6f4b182797c333b6821c891eb66d
root <root@VM_0_9_centos.(none)>
2018-03-09 17:19:14 +0800

    atexit示例

---
+++ chapter7/apue.h
@@ -0,0 +1,17 @@
+#ifndef _APUE_H
+#define _APUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXLINE 4096
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IXOTH)
+
+void err_dump(const char *, ...) __attribute__((noreturn));
+void err_quit(const char *, ...) __attribute__((noreturn));
+void err_sys(const char *, ...) __attribute__((noreturn));
+void err_ret(const char *, ...);
+
+#endif
